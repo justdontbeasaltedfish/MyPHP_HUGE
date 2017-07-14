566941bf4d133203622c5162ab3bf19dceedc169
WangJianrui <wangjianrui957@outlook.com>
2017-07-14 16:58:26 +0800

    Delete some useless files.

--- application/view/layouts/default_footer.php
+++ application/view/layouts/default_footer.php
@@ -1,2 +1,5 @@
+</div>
+<!--<script src="--><?php //echo Config::get('URL'); ?><!--js/jquery-3.2.1.js"></script>-->
+<!--<script src="--><?php //echo Config::get('URL'); ?><!--js/bootstrap.js"></script>-->
 </body>
 </html>
--- application/view/layouts/default_header.php
+++ application/view/layouts/default_header.php
@@ -2,5 +2,21 @@
 <html>
 <head>
     <title><?php echo (isset($this->title)) ? $this->title : '微博 App'; ?> - MyPHP_HUGE</title>
+    <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/app.css">
 </head>
 <body>
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="/" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/help">帮助</a></li>
+                    <li><a href="#">登录</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<div class="container">
